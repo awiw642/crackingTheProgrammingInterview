d36ccc987c31ce4ed214dc5ed7df34fbc5199d4c
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-11-19 21:52:57 +1300

    robot work in progress

--- eloquentJavascript/ch07/robot.js
+++ eloquentJavascript/ch07/robot.js
@@ -90,9 +90,20 @@ function randomRobot(state) {
     return {direction: randomPick(roadGraph[state.place])};
 }
 
+// start up the virtual world
 
+runRobot(VillageState.random(), randomRobot);
 
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House", "Town Hall", "Daria's House", "Ernie's House", "Grete's House", "Shop", "Grete's House", "Farm", "Marketplace", "Post Office"
+];
 
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute; 
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+}
 
 
 
