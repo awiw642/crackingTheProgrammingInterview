4eca9249ef9dda04c7b5d569f99341d57748c692
Aprianto Wiwarsono <awiwarsono@gmail.com>
2019-01-03 21:55:27 +1300

    Add stringCompression exercise

--- ch1/06-stringCompression/stringCompression.js
+++ ch1/06-stringCompression/stringCompression.js
@@ -1,24 +1,26 @@
-// @flow
 
 /*
 string compression of repeated chars. If the compressed string is not shorter than the original, return the original.
 */
-export default function stringCompression(string: string): string {
-  const compressedResult = string.split('').reduce((acc, curr) => {
-    if (curr === acc.currentChar) {
-      acc.currentCount++;
-    } if else (curr !== acc.currentChar) {
-      acc.compressedString = acc.currentCount > 1 ? acc.compressedString.concat(acc.currentChar, acc.currentCount) : acc.currentChar;
-    } else {
-      acc.currentChar = curr;
-      acc.currentCount = 1;
-    }
-    return acc;
-  }, {
-    currentChar: '',
-    currentCount: 0,
-    compressedString: '',
-  });
+export default function stringCompression(string) {
+    const collectedWords = [];
+    const current = {letter: '', count: 0};
 
-  compressedResult.compressedString.length < string ? return compressedResult.compressedString : return string;
+    for (let char of string) {
+        if (current.letter === '') {
+            current.letter = char;
+            current.count++;
+        } else if (current.letter === char) {
+            current.count++;
+        } else if (current.letter !== char) {
+            collectedWords.push(current.letter.concat(current.count));
+            current.letter = '';
+            current.count = 1;
+        }
+    }
+    collectedWords.push(current.letter.concat(current.count));
+    
+    const shortestString = collectedWords.join('').length < string.length ? collectedWords.join('') : string;
+    
+    return shortestString;
 }
--- ch1/06-stringCompression/stringCompression.test.js
+++ ch1/06-stringCompression/stringCompression.test.js
@@ -1,1 +1,10 @@
 import stringCompression from './stringCompression';
+
+test('stringCompression should always return the word with smallest numbers of characters', () => {
+    const word = 'dddeedddee';
+    const word2 = 'ddeeddee';
+    const compressedWord = 'd3e2d3e2'
+    const compressedWord2 = 'd2e2d2e2';
+    expect(stringCompression(word)).toBe(compressedWord);
+    expect(stringCompression(word2)).toBe(word2);
+});
